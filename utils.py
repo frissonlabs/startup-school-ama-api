8aa8c13b515a0ad3078e9bb4f4d624e3305b57fb
David Li <frissonlabs@gmail.com>
2023-01-30 01:46:59 -0800

    Updated prompt due inaccurate responses

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def get_context(index: pinecone.Index, embedded_query: list ):
 	result = index.query(embedded_query, top_k=12, include_metadata=True)
 	matches = result["matches"]
 	matches = [f"{m['metadata']['text']}" for m in matches if m['score'] > 0.5]
-	context = '. '.join(matches)
+	context = ' '.join(matches).replace('.,', '.')
 	return context
 
 def get_prompt(query: str, context: str):
@@ -14,7 +14,7 @@ def get_prompt(query: str, context: str):
 		Context:
 		{context}
 
-		Answer the following question as truthfully as possible using the provided text, and if the answer is not contained within the text below, say "I don't know": 
+		Answer the following question as truthfully as possible using the provided text: 
 		{query}
 		"""
 	return prompt
